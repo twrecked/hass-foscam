50b4e81f6b1c0434602d82b952f20dd8ff8c564b
Steve Herrell <steve.herrell@gmail.com>
2021-05-19 22:45:21 -0400

    Video library URL and listing.

---
+++ custom_components/foscam/media.py
@@ -0,0 +1,79 @@
+
+import os
+from datetime import datetime
+
+from .const import (
+    LOGGER
+)
+
+class Recording:
+
+    def __init__(self, date, recording, snapshot):
+
+        self._remote_recording = recording
+        self._remote_snapshot = snapshot,
+        self._date = date
+        self._duration = None
+
+        base = os.path.splitext(os.path.basename(recording))[0]
+        self._recording = f"/config/foscam/{base}.mp4"
+        self._snapshot = f"/config/foscam/{base}.jpg"
+        LOGGER.debug(f"Recording({self._recording})")
+
+    @property
+    def created_at(self):
+        """Returns date video was creaed."""
+        return self._date
+
+    def created_at_pretty(self, date_format=None):
+        """Returns date video was taken formated with `last_date_format`"""
+        if date_format:
+            return self._date.strftime(date_format)
+        return self._date().strftime("%Y-%m-%dT%H:%M:%S")
+
+    @property
+    def content_type(self):
+        return "video/mp4"
+
+    @property
+    def content_url(self):
+        return self._recording
+
+    @property
+    def remote_content_url(self):
+        return self._remote_recording
+
+    @property
+    def duration(self):
+        if self._duration is None:
+            if os.path.exists(self._recording):
+                result = subprocess.run(["ffprobe", "-v", "error", "-show_entries",
+                                            "format=duration", "-of",
+                                            "default=noprint_wrappers=1:nokey=1", self._recording],
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.STDOUT)
+                self._duration = int(result.stdout)
+                LOGGER.debug(f"duration of {self._remote_recording} is {self._duration}")
+        if self._duration:
+            return self._duration
+        return 1
+
+    @property
+    def thumbnail_type(self):
+        return "image/jpeg"
+
+    @property
+    def thumbnail_url(self):
+        return self._snapshot
+
+    @property
+    def remote_thumbnail_url(self):
+        return self._remote_snapshot
+
+    @property
+    def object_region(self):
+        return None
+
+    @property
+    def object_type(self):
+        return None
--- custom_components/foscam/updater.py
+++ custom_components/foscam/updater.py
@@ -8,9 +8,13 @@ from ftpretty import ftpretty
 from .const import (
     LOGGER
 )
+from .media import (
+    Recording
+)
 
-CACHE_CHECK = 2
+CHECK_TIMEOUT = 2
 RECORDINGS_TIMEOUT = 60
+TMP_AVI = "/config/foscam/in.avi"
 
 
 class Updater:
@@ -20,11 +24,14 @@ class Updater:
         self._hass = hass
         self._camera = camera
         self._last = 0
-        self._state = {}
-        self._recordings = {}
+        self._last_recording = 0
         self._todays_count = 0
         self._last_capture_at = None
         self._lock = threading.Lock()
+
+        # start up state
+        self._state = {}
+        self._recordings = []
 
     def get_datetime(self, filename):
         filename = os.path.splitext(filename)[0]
@@ -39,6 +46,7 @@ class Updater:
         return ret
 
     def update_recordings(self):
+
         res, devinfo = self._camera.get_dev_info()
         if res is not 0:
             LOGGER.error("failed to read device info")
@@ -55,49 +63,101 @@ class Updater:
         self._last_capture_at = None
 
         today = datetime.now().strftime("%Y%m%d")
+        url_base = f"ftp://{self._camera.usr}:{self._camera.pwd}@{self._camera.host}:50021/IPCamera"
 
         recordings = []
+        snapshots = {}
         for possible_dir in ftp.list("/IPCamera"):
             if mac in possible_dir:
-                for date in ftp.list(f"/IPCamera/{possible_dir}/record"):
-                    for date2 in ftp.list(f"/IPCamera/{possible_dir}/record/{date}"):
-                        for recording in ftp.list(f"/IPCamera/{possible_dir}/record/{date}/{date2}"):
+
+                # Save out the snapshots.
+                for date1 in ftp.list(f"/IPCamera/{possible_dir}/snap"):
+                    for date2 in ftp.list(f"/IPCamera/{possible_dir}/snap/{date1}"):
+                        for snapshot in ftp.list(f"/IPCamera/{possible_dir}/snap/{date1}/{date2}"):
+                            if not snapshot.endswith("jpg"):
+                                continue
+
+                            date = self.get_datetime(snapshot)
+                            snapshots[date] = f"/IPCamera/{possible_dir}/snap/{date1}/{date2}/{snapshot}"
+
+                # Build recordings array.
+                for date1 in ftp.list(f"/IPCamera/{possible_dir}/record"):
+                    for date2 in ftp.list(f"/IPCamera/{possible_dir}/record/{date1}"):
+                        for recording in ftp.list(f"/IPCamera/{possible_dir}/record/{date1}/{date2}"):
                             if not recording.endswith("avi"):
                                 continue
 
-                            recording_date = self.get_datetime(recording)
-                            if recording_date.strftime("%Y%m%d") == today:
+                            date = self.get_datetime(recording)
+
+                            # update counts
+                            if date.strftime("%Y%m%d") == today:
                                 self._todays_count += 1
-                            if self._last_capture_at is None or self._last_capture_at < recording_date:
-                                self._last_capture_at = recording_date
+                            if self._last_capture_at is None or self._last_capture_at < date:
+                                self._last_capture_at = date
 
-                            recordings.append({
-                                "type": "avi",
-                                "date": recording_date,
-                                "base": os.path.splitext(recording)[0],
-                                "path": f"/IPCamera/{possible_dir}/record/{date}/{date2}/{recording}",
-                                "url": f"ftp://{self._camera.usr}:{self._camera.pwd}@{self._camera.host}:50021"
-                                        f"/IPCamera/{possible_dir}/record/{date}/{date2}/{recording}"
-                            })
+                            # find a thumbnail
+                            snapshot = [k for k in sorted(snapshots.keys()) if k > date]
+                            if snapshot:
+                                snapshot = snapshots[snapshot[0]]
 
-        self._recordings = sorted(recordings, key=lambda x: x["base"])
+                            remote_recording = f"/IPCamera/{possible_dir}/record/{date1}/{date2}/{recording}"
+                            recordings.append(Recording(date, remote_recording, snapshot))
+
+        ftp.close()
+
+        self._recordings = sorted(recordings, key=lambda x: x.created_at, reverse=True)
+
         return 0
+
+    def fetch_recordings(self):
+
+        for recording in self._recordings:
+            if not os.path.exists(recording.content_url):
+                self._camera.execute_command('startFtpServer')
+                ftp = ftpretty(self._camera.host, self._camera.usr, self._camera.pwd, port=50021)
+
+                LOGGER.debug(f"copying {recording.thumbnail_url}")
+                ftp.get(recording.remote_thumbnail_url, recording.thumbnail_url)
+
+                LOGGER.debug(f"creating {recording.content_url}")
+                ftp.get(recording.remote_content_url, TMP_AVI)
+                os.system(f"ffmpeg -i {TMP_AVI} {recording.content_url}")
+                os.unlink(TMP_AVI)
+
+                ftp.close()
+                return
 
     def update_data(self):
         """Fetch data from camera endpoint
         """
         with self._lock:
             now = time.monotonic()
-            if self._last + CACHE_CHECK > now:
-                LOGGER.debug("using cache")
+
+            # too soon?
+            if self._last + CHECK_TIMEOUT > now:
+                LOGGER.debug(f"too soon to update {self._last} -- {now}")
                 return
+
+            # save pre-update state
+            motion_detected = self._state.get("motionDetectAlarm", "0") == "2"
 
             LOGGER.debug("update state")
             self.update_state()
 
-            if (self._last + RECORDINGS_TIMEOUT) < now:
+            # check post-update state
+            if motion_detected and self._state.get("motionDetectAlarm", "0") != "2":
+                LOGGER.debug("force recording update")
+                self._last_recording = 0
+
+            # check recordings
+            if (self._last_recording + RECORDINGS_TIMEOUT) < now:
                 LOGGER.debug("update recordings")
                 self.update_recordings()
+                self._last_recording = now
+
+            # grab one recording per go after initial setup
+            if self._last != 0:
+                self.fetch_recordings()
 
             self._last = now
 
